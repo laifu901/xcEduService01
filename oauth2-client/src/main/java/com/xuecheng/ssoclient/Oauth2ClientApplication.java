ad69259e23eae74db45c73a8de27edd1d7920bcf
laifu901 <1220070007@qq.com>
2020-04-15 14:53:26 +0800

    Initial commit

---
+++ oauth2-client/src/main/java/com/xuecheng/ssoclient/Oauth2ClientApplication.java
@@ -0,0 +1,16 @@
+package com.xuecheng.ssoclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+
+@EnableOAuth2Sso
+@SpringBootApplication
+public class Oauth2ClientApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Oauth2ClientApplication.class, args);
+    }
+
+}
